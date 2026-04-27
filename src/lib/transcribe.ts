fb5ff0f9ee9d466072d831cb818a652c43ba07fb
winnie334 <winnie33winnie33@gmail.com>
2026-04-27 17:55:47 +0200

    Add trimmed pdfs

--- src/lib/transcribe.ts
+++
@@ -1,8 +0,0 @@
-export async function transcribe(_file: File): Promise<string[]> {
-    //      const form = new FormData(); form.append('file', _file);
-    //      const res = await fetch('/api/transcribe', { method: 'POST', body: form });
-    //      return res.json(); // string[]
-
-    await new Promise((r) => setTimeout(r, 600));
-    return ["Very solid transcription"];
-}
